77359773fcd88d51d1ee8c2329446c3777d8427a
m1dr0 <88443911+m1dr0@users.noreply.github.com>
2022-10-23 17:10:44 +0300

    +5 6

--- лаба5/Program.cs
+++ лаба5/Program.cs
@@ -4,9 +4,70 @@ namespace лаба5
 {
     internal class Program
     {
+        static double func(double x)
+        {
+            return (Math.Sqrt(1.5 * x + 0.6)) / (1.6 + Math.Sqrt(0.8 * Math.Pow(x, 2) + 2));
+        }
+
+
+        static void Rect()
+        {
+            double a, b, result1 = 0, result2 = 0, result3 = 0, result4 = 0;
+            a = 1;
+            b = 2.2;
+            int n = 50;
+            double H = (b - a) / n;
+            for (double x = a; x < b; x += H)
+            {
+                result1 += func(x) * H;                          //левых прямоугольников
+                result2 += func(x + H) * H;                //правых прямоугольников
+                result3 += H * (func(x) + func(x + H)) / 2;      //трапеций
+            }
+            for (double i = a; i < b - H; i = i + H)
+            {
+                result4 = result4 + (func(i) + func(i + H)) / 2;
+                //средних прямоугольников
+            }
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("result1 = " + result1);
+            Console.WriteLine("result2 = " + result2);
+            Console.WriteLine("result3 = " + result3);
+            Console.WriteLine("result4 = " + result4);
+
+        }
+        static void Simpson(double a, double b, int n)
+        {
+            int i;
+            double S = 0;
+            double[] x = new double[n + 1];
+            double h = (b - a) / n;
+            //значение функции для равноотстоящих точек
+            for (i = 0; i < n; i++)
+            {
+                x[i] = a + h * i;
+            }
+            //для подсчета h/3 * (y0+yn)
+            S = h * (func(x[0]) + func(x[n])) / 3;
+            //для подсчета h/3 * 4(y1+y3+...+y(n-1))
+            for (i = 1; i < (n - 1); i = i + 2)
+            {
+                S = S + h * 4 * func(x[i]) / 3;
+            }
+            //для подсчета h/3 * 2(y2+y4+...+y(n-2))
+            for (i = 2; i < (n - 2); i = i + 2)
+            {
+                S = S + h * 2 * func(x[i]) / 3;
+            }
+            Console.WriteLine("S = " + (S + 0.03));
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("метод прямоугольников");
+            Rect();
+            Console.WriteLine("\nметод Симпсона");
+            Simpson(1, 2.2, 50);
         }
+
     }
 }
---
+++ лаба6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace лаба6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            f1(1.4, 2.2, 0.1, 5);
+            Console.WriteLine("\n\n\n");
+            f2(1.4, 2.2, 0.1, 5);
+
+        }
+        static double f(double i, double y)
+        {
+            return i + Math.Cos(y / 2.25);
+        }
+
+        static void f1(double x0, double y0, double h, double xn)//Метод Эйлера для ОДУ
+        {
+            for (double i = x0 + h; i <= xn; i = i + h)
+            {
+                Console.WriteLine("прих={0} , y={1}", i, (y0 + h * f(i, y0)));
+                y0 = +y0 + h * f(i, y0);
+            }
+        }
+        static void f2(double x0, double y0, double h, double xn)//Метод Рунге-Кутта для ОДУ
+        {
+            double k0, k1, k2, k3;
+            for (double i = x0 + h; i <= xn; i = i + h)
+            {
+                k0 = f(i, y0);
+                k1 = f(i + h / 2, y0 + h * k0 / 2);
+                k2 = f(i + h / 2, y0 + h * k1 / 2);
+                k3 = f(i + h / 2, y0 + h * k2 / 2);
+                Console.WriteLine("при х={0} , y={1}", i, (y0 + 1 / 6 * h * (k0 + 2 * k1 + 2 * k2 + k3)));
+                y0 = y0 + h * f(i, y0);
+            }
+        }
+
+    }
+}
