e1aa5df9d9ba62d92804858a2c3a7ce34140d120
m1dr0 <88443911+m1dr0@users.noreply.github.com>
2022-10-28 01:31:22 +0300

    6 laba

--- лаба6/Program.cs
+++ лаба6/Program.cs
@@ -4,39 +4,91 @@ namespace лаба6
 {
     internal class Program
     {
+        static void StartProgramm()
+        {
+            Console.WriteLine("1.Метод Эйлера системы");
+            Console.WriteLine("2.Метод Рунге-Кутта для системы");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    TestEulerCoupledODE();
+                    break;
+                case 2:
+                    run();
+                    break;
+                default:
+                    StartProgramm();
+                    break;
+
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+            StartProgramm();
+        }
+
+        static double fi(double x, double y, double z)
+        { 
+            return -2 * y * x + z; 
+        }
+
+        static double tu(double x, double y, double z)
+        { 
+            return 3 * y - 2 * z; 
+        }
+
+
+        static void TestEulerCoupledODE()
+        {
+            double x = 0, y = 5.0, z = 10.0;  //initial conditions
+            double h = 0.11;                  //step size
+            double k0, k1, k2, k3;
+            double l0, l1, l2, l3;
+
+            for (double i = 0 + h; i <= 1; i = i + h)
+            {
+
+                k0 = fi(x, y, z) * h;
+                l0 = tu(x, y, z) * h;
+                k1 = fi(x + h / 2, y + k0 / 2, z + l0 / 2) * h;
+                l1 = tu(x + h / 2, y + k0 / 2, z + l0 / 2) * h;
+                k2 = fi(x + h / 2, y + k1 / 2, z + l1 / 2) * h;
+                l2 = tu(x + h / 2, y + k1 / 2, z + l1 / 2) * h;
+                k3 = fi(x + h, y + k2, z + l2) * h;
+                l3 = tu(x + h, y + k2, z + l2) * h;
+                y = y + (k0 + 2 * k1 + 2 * k2 + k3) / 6;
+                z = z + (l0 + 2 * l1 + 2 * l2 + l3) / 6;
+            }
+            Console.WriteLine("y = {0} z = {1}", y, z);
+        }
+
+        static void run()
+        {
+            double x = 0, y = 5.0, z = 10.0;  //initial conditions
+            double h = 0.1;                  //step size
+            double k0, k1, k2, k3;
+            double l0, l1, l2, l3;
+
+            for (double i = 0 + h; i <= 1; i = i + h)
+            {
+
+                k0 = fi(x, y, z) * h;
+                l0 = tu(x, y, z) * h;
+                k1 = fi(x + h / 2, y + k0 / 2, z + l0 / 2) * h;
+                l1 = tu(x + h / 2, y + k0 / 2, z + l0 / 2) * h;
+                k2 = fi(x + h / 2, y + k1 / 2, z + l1 / 2) * h;
+                l2 = tu(x + h / 2, y + k1 / 2, z + l1 / 2) * h;
+                k3 = fi(x + h, y + k2, z + l2) * h;
+                l3 = tu(x + h, y + k2, z + l2) * h;
+                y = y + (k0 + 2 * k1 + 2 * k2 + k3) / 6;
+                z = z + (l0 + 2 * l1 + 2 * l2 + l3) / 6;
+            }
+            Console.WriteLine("y = {0} z = {1}", y, z);
+        }
+
+
         static void Main(string[] args)
         {
-            f1(1.4, 2.2, 0.1, 5);
-            Console.WriteLine("\n\n\n");
-            f2(1.4, 2.2, 0.1, 5);
-
+            StartProgramm();
         }
-        static double f(double i, double y)
-        {
-            return i + Math.Cos(y / 2.25);
-        }
-
-        static void f1(double x0, double y0, double h, double xn)//Метод Эйлера для ОДУ
-        {
-            for (double i = x0 + h; i <= xn; i = i + h)
-            {
-                Console.WriteLine("прих={0} , y={1}", i, (y0 + h * f(i, y0)));
-                y0 = +y0 + h * f(i, y0);
-            }
-        }
-        static void f2(double x0, double y0, double h, double xn)//Метод Рунге-Кутта для ОДУ
-        {
-            double k0, k1, k2, k3;
-            for (double i = x0 + h; i <= xn; i = i + h)
-            {
-                k0 = f(i, y0);
-                k1 = f(i + h / 2, y0 + h * k0 / 2);
-                k2 = f(i + h / 2, y0 + h * k1 / 2);
-                k3 = f(i + h / 2, y0 + h * k2 / 2);
-                Console.WriteLine("при х={0} , y={1}", i, (y0 + 1 / 6 * h * (k0 + 2 * k1 + 2 * k2 + k3)));
-                y0 = y0 + h * f(i, y0);
-            }
-        }
-
     }
 }
